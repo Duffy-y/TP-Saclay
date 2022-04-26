5b6fdb37a85ed8590be4424c824db3dd26f0b1d1
Duffy <alexxandre.mnd+dev@gmail.com>
2022-04-26 19:13:38 +0200

    Fin expérience 2

--- MethExp_TP4/main.py
+++ MethExp_TP4/main.py
@@ -5,6 +5,7 @@ from scipy.odr import *
 from tp_lib import pente_extreme, incertitude_derivee_partielle
 from sympy import *
 import pandas as pd
+from sklearn.metrics import r2_score
 
 def linear(x, a, b):
     return a * x + b
@@ -25,10 +26,34 @@ dP = 1 * 10**(2) # Pa
 data_exp2_chauffage = pd.read_csv("data/Exp2Chauffage.csv", delimiter=";")
 data_exp2_refroidissement = pd.read_csv("data/Exp2Refroidissement.csv", delimiter=";")
 
-plt.plot(data_exp2_chauffage["Température (K)"], data_exp2_chauffage["Pression (hPa)"], 'ro', label="Chauffage")
-plt.plot(data_exp2_refroidissement["Température (K)"], data_exp2_refroidissement["Pression (hPa)"], "bo", label="Refroidissement")
+pression_chauffage = data_exp2_chauffage["Pression (hPa)"]
+temperature_chauffage = data_exp2_chauffage["Température (K)"]
+pression_refroid = data_exp2_refroidissement["Pression (hPa)"]
+temperature_refroid = data_exp2_refroidissement["Température (K)"]
+
+
+def linear(x, a, b):
+    return a * x + b
+    
+popt, pcov = curve_fit(linear, temperature_chauffage, pression_chauffage, sigma=pression_chauffage * 0.01, absolute_sigma=True)
+perr = np.sqrt(np.diag(pcov))
+print(perr)
+
+pression_reg = popt[0] * temperature_chauffage + popt[1]
+
+fig = plt.figure(figsize=(10, 5))
+plt.errorbar(temperature_chauffage, pression_chauffage, yerr=pression_chauffage * 0.01, xerr=temperature_chauffage * 0.001 , fmt='o', label="Chauffage", zorder=-1, c="tab:red")
+#plt.errorbar(temperature_refroid, pression_refroid, yerr=pression_refroid * 0.01, xerr=temperature_refroid * 0.001, fmt="bo", label="Refroidissement")
+plt.plot(temperature_chauffage, pression_reg, "-", label="Régression linéaire", zorder=2, linewidth=3, c="tab:green")
+
+print(f"R² score = {r2_score(pression_chauffage, pression_reg)}")
+print(f"a = {popt[0]:.2f} ± {perr[0]:.2f}, b = {popt[1]:.2f} ± {perr[1]:.2f}")
+
+print(f"{-popt[1]/popt[0]}")
+print(f"{perr[1]/popt[0] + popt[1]/popt[0]**2 * perr[0]}")
 
 plt.legend()
 plt.xlabel("Température [K]")
 plt.ylabel("Pression [hPa]")
+#plt.savefig("img/exp2_graph2.eps", format="eps")
 plt.show()
