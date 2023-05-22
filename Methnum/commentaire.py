b421f560bc78cecdb00ed3b0b26ca78c8e2541cc
Alexandre MENARD <alexxandre.mnd+dev@gmail.com>
2023-05-22 23:55:50 +0200

    Modif animation

---
+++ Methnum/commentaire.py
@@ -0,0 +1,9 @@
+#y_axes=ax.set_ylim(0,top=potential(-2,0))
+
+#def ini():
+#    plot.set_xdata(0)
+#    plot.set_ydata(0)
+#    ax.set_ylabel('')
+#    ax.set_xlabel('x')
+#    t_text=('0')
+#    return plot,t_text
